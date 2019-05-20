1d77592c7e906f64288196ed7e3f0bbd6b1d4025
Klaus Meyer <spam@klaus-meyer.net>
2019-05-20 15:17:24 +0200

    validate repository clone url against existing regexp

--- app/models/repository.rb
+++ app/models/repository.rb
@@ -1,6 +1,8 @@
 class Repository < ApplicationRecord
+  URL_REGEX = /\A(?<user>.+)@(?<server>.[^:]+):(?<path>.+).git\z/
+
   validates :name, presence: true
-  validates :url,  presence: true
+  validates :url,  presence: true, format: { with: URL_REGEX }
 
   has_many :jobs, dependent: :destroy
 
@@ -15,8 +17,4 @@ class Repository < ApplicationRecord
   def path
     url.match(URL_REGEX)[:path]
   end
-
-  private
-
-  URL_REGEX = /@(?<server>.[^:]+):(?<path>.+).git$/
 end
